b1e54ab1c720abd23507c78c8b4b9f26276342a5
Tarako0329 <86461908+Tarako0329@users.noreply.github.com>
2024-06-21 18:30:16 +0900

    ver1.01.0:アニメーション変更

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ $time=date('Ymd-His');
 	<meta name="google-site-verification" content="Ti3a9WFQOPYTR_HHzZ2jtft_iw_RN3xK_ydPCS0EpfQ" />
 	<meta name="description" content="webで遊べる！すぐに遊べる！無料で無制限タイピングゲーム。こどもから大人まで、気軽にどうぞ～">
 	<link rel='apple-touch-icon' href='apple-touch-icon.png'>
+	<link rel='icon' href='favicon.ico'>
 	<!-- Bootstrap5 CSS/js -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
